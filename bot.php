f2b4e3a9483fa4276cf0c045be969a3e0bdd291d
warakornj <kornzaaz10@gmail.com>
2020-03-04 10:43:14 +0700

    line crop

--- bot.php
+++ bot.php
@@ -65,7 +65,7 @@ $replyToken = $event['replyToken'];
 // Build message to reply back
 $messages = [
 'type' => 'text',
-'text' => $text
+'text' => $text."ok"
 ];
 // Make a POST Request to Messaging API to reply to sender
 $url = 'https://api.line.me/v2/bot/message/reply';

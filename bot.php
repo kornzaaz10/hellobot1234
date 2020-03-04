9e5797b73eee18f48d8a96131384a6481227959d
warakornj <kornzaaz10@gmail.com>
2020-03-04 10:45:48 +0700

    line crop

--- bot.php
+++ bot.php
@@ -65,7 +65,7 @@ $replyToken = $event['replyToken'];
 // Build message to reply back
 $messages = [
 'type' => 'text',
-'text' => $text."ok"
+'text' => $text
 ];
 // Make a POST Request to Messaging API to reply to sender
 $url = 'https://api.line.me/v2/bot/message/reply';
@@ -83,7 +83,7 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 $result = curl_exec($ch);
 curl_close($ch);
-echo $result . "\r\n";
+echo $result . "ok\r\n";
 }
 }
 }
